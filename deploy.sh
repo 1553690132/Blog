264d2423e139b75714b3a7524c18fd951025a3b7
1553690132 <1553690132@qq.com>
2022-11-08 17:43:51 +0800

    action

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,18 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+npm run build
+
 # 进入生成的文件夹
 cd ./public
 
 git init
 git add -A
-git commit -m '--first commit'
+git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>(REPO就是之前创建的仓库名)
+git remote add origin git@github.com:1553690132/blog.git
+git branch -M main
 git push -f git@github.com:1553690132/blog.git main:gh-pages
 
 cd -
