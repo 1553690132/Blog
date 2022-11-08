879df925ae6bfbb7f58dd1fb63311bb1a59f45aa
1553690132 <1553690132@qq.com>
2022-11-08 17:34:24 +0800

    1

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,16 @@
-#!/usr/bin/env sh
-
+# deploy.sh文件
 # 确保脚本抛出遇到的错误
 set -e
 
-# 生成静态文件
-npm run docs:build
-
 # 进入生成的文件夹
-cd .vuepress/dist
-
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+cd ./public
 
 git init
-git add .
-git commit -m 'deploy'
+git add -A
+git commit -m '--first commit'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git remote add origin git@github.com:1553690132/blog.git
-git branch -M main
+# 如果发布到 https://<USERNAME>.github.io/<REPO>(REPO就是之前创建的仓库名)
 git push -f git@github.com:1553690132/blog.git main:gh-pages
 
 cd -
+
