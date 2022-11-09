fc0291b041a9112c70cb6a7035575cf8dd8cb077
1553690132 <1553690132@qq.com>
2022-11-09 09:21:55 +0800

    bgm

--- .vuepress/config/pluginsConf.js
+++ .vuepress/config/pluginsConf.js
@@ -4,4 +4,14 @@ module.exports = {
         shape: 'circle',
         zIndex: 999999999
     },
+    "@vuepress-reco/vuepress-plugin-bgm-player": {
+        audios: [
+            {
+                name: '等你下课',
+                artist: '周杰伦',
+                url: '/bgm/周杰伦 - 等你下课 (with 杨瑞代).mp3',
+                cover: '/bgm/music_main.jpg'
+            },
+        ]
+    }
 }
