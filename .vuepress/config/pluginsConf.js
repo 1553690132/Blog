63c5be85fe9c22fc3f3af0344522af85b6dda25f
1553690132 <1553690132@qq.com>
2022-11-09 09:47:59 +0800

    x

--- .vuepress/config/pluginsConf.js
+++ .vuepress/config/pluginsConf.js
@@ -12,6 +12,12 @@ module.exports = {
                 url: '/blog/bgm/周杰伦 - 等你下课 (with 杨瑞代).mp3',
                 cover: '/blog/bgm/music_main.jpg'
             },
+            {
+                name: '粉色海洋',
+                artist: '周杰伦',
+                url: '/blog/bgm/周杰伦 - 粉色海洋.mp3',
+                cover: '/blog/bgm/music_main.jpg'
+            },
         ]
     }
 }
